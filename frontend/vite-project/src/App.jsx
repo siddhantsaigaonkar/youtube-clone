41a80dc3370911647b8fdd67d466d81d156819df
siddhantsaigaonkar <siddhantsaigaonkar14k@gmail.com>
2026-08-10 20:28:04 +0530

    Set up React routing

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      App
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+   
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
