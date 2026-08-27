9d4c5ae80e2bfcf5daa8d07dc4ebbf53c8aa8704
siddhantsaigaonkar <siddhantsaigaonkar14k@gmail.com>
2026-08-27 23:13:43 +0530

    feat(subscription): add subscription model

---
+++ backend/models/subscriptionModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Channel",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+subscriptionSchema.index({ subscriber: 1, channel: 1 }, { unique: true });
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+export default Subscription;
