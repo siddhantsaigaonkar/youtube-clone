b3c743fbbf0f08611df6847dea795a0fe0f00f4e
siddhantsaigaonkar <siddhantsaigaonkar14k@gmail.com>
2026-08-27 23:17:28 +0530

    refactor(frontend): update application entry point

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import ThemeProvider from './context/ThemeContext.jsx'
+import  AuthProvider from "./context/AuthContext.jsx"
+ThemeProvider
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </ThemeProvider>,
+);
